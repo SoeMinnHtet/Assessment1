93accf27fdc90bd3dab483f14140730e6797317d
Soe Minn Htet <c3249045@uon.edu.au>
2019-04-02 02:46:09 +0000

    1

--- assessment1.c
+++ assessment1.c
@@ -1,1 +1,20 @@
-hello world
+#include <stdio.h>
+#include <math.h> 
+
+
+int main()
+{
+    int N=50;
+    int k; // Loop Counter
+    float x1=0,x2=2 ;
+    float dx=(x2-x1) / N ;
+    
+    float sum=0;
+    for (k=1;k<N;k++) {
+        float x;
+        x = x1 + k*dx ;
+        sum = sum + x;
+    }
+    printf (" %f", sum);
+    
+}
