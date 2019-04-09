693fafed50d6347db0175fb01d1595abef6b6243
Soe Minn Htet <c3249045@uon.edu.au>
2019-04-09 02:12:23 +0000

    new

--- assessment1.c
+++ assessment1.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <math.h> 
+#include <stdlib.h>
+#include <string.h>
+
+// Functions
+void Cypher (char* plainText, int k);
 
 
-int main()
-{
-    int N=50;
-    int k; // Loop Counter
-    float x1=0,x2=2 ;
-    float dx=(x2-x1) / N ;
+int main(void) {
+    int k = 1;      // Key
+    char plainText[101] ;    
+    printf (" Enter Text that you want to encrypt : ");
+    fgets(plainText, sizeof(plainText),stdin);
     
-    float sum=0;
-    for (k=1;k<N;k++) {
-        float x;
-        x = x1 + k*dx ;
-        sum = sum + x;
+    
+    printf ("The cypher text is :");
+    // insert the function here
+    Cypher (plainText, k);
+    
+    
+ 
+}
+// Function Expression
+void Cypher (char* plainText2, int k1) {
+    int i = 0 ;
+    int cypherValue ;
+    char cypher;
+    
+    while (plainText2[i] != '\0' && strlen(plainText2)-1 > i) {
+        cypherValue = ((int)plainText2[i] -97 + k1)%26 + 97 ;
+        cypher = (char)(cypherValue);
+        
+        printf ("%c" , cypher);
+        i++;
+        
     }
-    //iyuiyiuiuio
-    printf (" %f", sum);
-    printf (" %d\n", N);//  hello there
+    printf ("\n");
 }
